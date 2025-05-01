8c84534497d66a065cf60e093fdc41a4724f529b
bananackck <emilly03141221@gmail.com>
2025-05-01 15:59:14 +0900

    (Feat) make login page structure

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -5,22 +5,17 @@ export default function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col max-w-[600px] w-full mx-auto bg-background shadow-sides">
       {/* 공통 Header */}
-      <Header type="main" point={999}/>
-      {/* <Header type="login" point={999} />
-      <Header type="down" point={999} />
-      <Header type="downLogin" point={999} />
-      <Header type="nav" point={999} />
-      <Header type="minimal" point={999} /> */}
+      {/* <Header type="main" point={999}/> */}
+      <Header type="minimal"/>
+      {/* <Header type="down" point={999} /> */}
+      {/* <Header type="downLogin" point={999} /> */}
+      {/* <Header type="nav" point={999} /> */}
+      {/* <Header type="minimal" point={999} /> */}
 
       {/* 각 페이지가 여기에 끼워진다 */}
-      <main className="flex-1 p-4">
+      <main className="flex-1 flex flex-col items-center justify-center p-4 ">
         <Outlet />
       </main>
-
-      {/* 공통 Nav */}
-      <footer className="h-16  w-full bg-gray-100 flex items-center justify-center">
-        <p className="text-sm text-gray-500">© 2025 CareerBee</p>
-      </footer>
     </div>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,19 @@
+import {Button} from "@/components/ui/button";
+
 export default function Login(){
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold mb-4">로그인 페이지</h1>
-        <p className="text-lg">이 페이지는 아직 개발 중입니다.</p>
+      // <div></div>
+        <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-center text-2xl font-bold ">
+          No.1 AI기반 커리어 길찾기 서비스<br />
+          커리어비에 오신 것을<br />
+          환영합니다! 
+        </h1>  
+        <p className="text-center text-lg">
+          지금 바로 로그인하고 <br />
+          맞춤형 서비스를 경험해보세요
+        </p>
+        <Button label="카카오톡으로 시작하기" size="lg" fullWidth={true} className="mx-16 mb-8"></Button>
       </div>
     );
 }
