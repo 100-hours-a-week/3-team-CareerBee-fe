44dedfcf295aa69b261731f6d72659fb2d3e1dcf
bananackck <emilly03141221@gmail.com>
2025-05-01 20:45:21 +0900

    (Refact) compact navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,23 +1,26 @@
 import { PiBookOpenText, PiMedalMilitary, PiMapTrifold, PiShoppingCartSimple, PiUser } from "react-icons/pi";
 
+interface NavItem {
+    name: string;
+    href: string;
+    icon: React.ReactNode;
+};
+const navItems: NavItem[] = [
+    { name: 'Interview', href: '/interview', icon: <PiBookOpenText className="iconSize-default" /> },
+    { name: 'Ranking', href: '/competition', icon: <PiMedalMilitary className="iconSize-default" /> },
+    { name: 'Home', href: '/', icon: <PiMapTrifold className="iconSize-default" /> },
+    { name: 'Store', href: '/store', icon: <PiShoppingCartSimple className="iconSize-default" /> },
+    { name: 'My Page', href: '/my', icon: <PiUser className="iconSize-default" /> },
+];
+
 export const Navbar = () => {
-    return(
-    <div className="flex items-center justify-between px-4 h-16 w-full gap-2">
-        <a href="/interview">
-            <PiBookOpenText className="w-8 h-8" />
-        </a>
-        <a href="/competition">
-            <PiMedalMilitary className="w-8 h-8" />
-        </a>
-        <a href="/">
-            <PiMapTrifold className="w-8 h-8" />
-        </a>
-        <a href="/store">
-            <PiShoppingCartSimple className="w-8 h-8" />
-        </a>
-        <a href="/my">
-            <PiUser className="w-8 h-8" />
-        </a>
-    </div>
-    )
+    return (
+        <div className="flex items-center justify-between px-4 h-16 w-full gap-2">
+            {navItems.map((item) => (
+                <a key={item.name} href={item.href}>
+                    {item.icon}
+                </a>
+            ))}
+        </div>
+    );
 }
