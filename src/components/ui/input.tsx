2a973b36ef5f6d7602391bba76e89c9485285dc3
bananackck <emilly03141221@gmail.com>
2025-05-05 03:28:17 +0900

    (Feat) add default and search type to input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,23 +1,50 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import { cva } from "class-variance-authority"
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, maxLength, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          className
-        )}
-        ref={ref}
-        maxLength={maxLength}
-        {...props}
-      />
-    )
-  }
-)
+const SearchVariants = cva(
+  `flex w-full border placeholder:text-muted-foreground text-base`,
+  {
+    variants:{
+      variant:{
+        default: `h-9 rounded-md border-input 
+        bg-transparent px-3 py-1
+        file:border-0 file:bg-transparent 
+        file:text-sm file:font-medium 
+        file:text-foreground 
+        focus-visible:outline-none 
+        focus-visible:ring-1 
+        focus-visible:ring-ring 
+        disabled:cursor-not-allowed 
+        disabled:opacity-50 
+        md:text-sm`,
+
+        search: `h-11 rounded-full border-border bg-white pl-10 pr-10 `
+      },
+    },
+    defaultVariants:{
+      variant:"default"
+    },
+  },
+);
+
+const Input = React.forwardRef<
+  HTMLInputElement,
+  React.ComponentProps<"input"> & { variant?: "default" | "search" }
+>(({ className, type, maxLength, variant = "default", ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        SearchVariants({variant}), className
+      )}
+      ref={ref}
+      maxLength={maxLength}
+      {...props}
+    />
+  )
+})
 Input.displayName = "Input"
 
 export { Input }
