44476c1413f99df6ab7c0f31477c3ce5f032884d
bananackck <emilly03141221@gmail.com>
2025-05-09 13:49:31 +0900

    (Feat) restore token on app refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@/router';
+import { useEffect } from 'react';
+import { useAuthStore } from '@/store/auth';
+
 
 function App() {
+  //토큰 복원
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      useAuthStore.getState().setToken(token);
+    }
+  }, []);
+
   return <RouterProvider router={router} />;
 }
 
