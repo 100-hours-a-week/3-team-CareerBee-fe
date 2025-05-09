233f39fefe58ee2d3d09d46909259c5336ca4b2f
bananackck <emilly03141221@gmail.com>
2025-05-09 23:47:47 +0900

    (Design) change recruit design

--- src/components/domain/company/recruit.tsx
+++ src/components/domain/company/recruit.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 export default function RecruitTab({ recruitments }: Props) {
     return (
-      <div>
+      <div className="px-2">
       <div className="flex gap-2 py-2 [&_svg]:size-5">
         <PiBriefcase /> 
         <p>현재 채용 정보</p>
@@ -15,14 +15,14 @@ export default function RecruitTab({ recruitments }: Props) {
       ? (
         <ul className="space-y-2 px-7">
           { recruitments.map((recruitment) => (
-            <li key={recruitment.id} className="border p-3 rounded-md">
+            <li key={recruitment.id} className="flex gap-1 items-start">
+              <p className="whitespace-nowrap">[{recruitment.title}]</p>
               <a
                 href={recruitment.url}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-blue-600 font-semibold underline"
-              >
-                {recruitment.title}
+                className="text-text-primary underline hover:text-text-primary/60 truncate">
+                {recruitment.url}
               </a>
             </li>
           ))}
