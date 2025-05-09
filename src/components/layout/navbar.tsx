563663da4b5ce3915e76632253e2a6cc210bc50e
bananackck <emilly03141221@gmail.com>
2025-05-09 14:36:22 +0900

    (Feat) make login-required page & handle mypage redirection

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,25 +1,24 @@
-import {
-  PiBookOpenText,
-  PiMedalMilitary,
-  PiMapTrifold,
-  PiShoppingCartSimple,
-  PiUser,
-} from 'react-icons/pi';
-
+import { PiBookOpenText, PiMedalMilitary, PiMapTrifold, PiShoppingCartSimple, PiUser } from 'react-icons/pi';
+import { useAuthStore } from '@/store/auth';
 interface NavItem {
   name: string;
   href: string;
   icon: React.ReactNode;
 }
-const navItems: NavItem[] = [
+const getNavItems = (token: string | null): NavItem[] => [
   { name: 'Interview', href: '/interview', icon: <PiBookOpenText className="iconSize-default" /> },
   { name: 'Ranking', href: '/competition', icon: <PiMedalMilitary className="iconSize-default" /> },
   { name: 'Home', href: '/', icon: <PiMapTrifold className="iconSize-default" /> },
   { name: 'Store', href: '/store', icon: <PiShoppingCartSimple className="iconSize-default" /> },
-  { name: 'My Page', href: '/my', icon: <PiUser className="iconSize-default" /> },
+  ...(token
+    ? [{ name: 'My Page', href: '/my', icon: <PiUser className="iconSize-default" /> }]
+    : [{ name: 'Login Required', href: '/login-required', icon: <PiUser className="iconSize-default" /> }]),
 ];
 
 export const Navbar = () => {
+  const token = useAuthStore.getState().token;
+  const navItems = getNavItems(token);
+
   return (
     <div className="flex items-center justify-between px-8 h-16 w-full gap-2 bg-background">
       {navItems.map((item) => (
