3531216c87247c54f47b27efd4f02626dc07d3e6
bananackck <emilly03141221@gmail.com>
2025-05-11 21:41:05 +0900

    (Feat) publish recruiting layout

--- src/pages/CompanyDetail.tsx
+++ src/pages/CompanyDetail.tsx
@@ -112,6 +112,9 @@ export default function CompanyDetail() {
 
   return (
     <div className="flex flex-col grow">
+      <div className="flex items-center justify-center h-6 bg-secondary text-sm text-text-primary leading-none py-2">
+        <p>현재 채용 중 입니다.</p>
+      </div>
       {/* 갤러리 */}
       <div className="grid grid-cols-4 grid-rows-2 gap-1 max-w-full mx-auto">
       {[...Array(5)].map((_, index) => {
