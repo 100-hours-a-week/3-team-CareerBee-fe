6c40eaf8a20947739f67100377f31ef7b6e4412c
bananackck <emilly03141221@gmail.com>
2025-05-13 13:59:40 +0900

    (Fix) fix clusterer and alone marker overlapped error&resize clusterer

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -154,7 +154,7 @@ export default function Main() {
         (marker) => marker.constructor.name !== 'I'
       );
   
-      if (realMarkers.length === 2 && cluster._clusterMarker) {
+      if (realMarkers.length === markers.length-1 && cluster._clusterMarker) {
         // 1. 클러스터 마커 숨기기
         cluster._clusterMarker.setVisible(false);
   
@@ -165,7 +165,10 @@ export default function Main() {
       }
     });
   };
+
   const [highlightedCompanyId, setHighlightedCompanyId] = useState<number | null>(null);
+
+
   return (
     <>
       <Toaster />
@@ -212,62 +215,62 @@ export default function Main() {
             onDragEnd={handleMapMove}
           >
             <MarkerClusterer
-  averageCenter={true}
-  minLevel={4}
-  minClusterSize={3}
-  onCreate={onCreate}
-  calculator={[10, 30, 50, 100]}
-  styles={[
-    {
-      width: '30px',
-      height: '30px',
-      background: 'radial-gradient(circle, rgba(26, 143, 227, 0.8) 40%, rgba(0, 0, 0, 0) 100%', // deep blue
-      borderRadius: '50%',
-      color: '#fff',
-      textAlign: 'center',
-      lineHeight: '30px',
-      fontSize: '13px',
-      fontWeight: 'bold',
-      boxShadow: '0 0 6px 4px rgba(26, 143, 227, 0.3)',
-    },
-    {
-      width: '40px',
-      height: '40px',
-      background: 'radial-gradient(circle, rgba(93, 162, 113, 0.8) 40%, rgba(0, 0, 0, 0) 100% )', // teal
-      borderRadius: '50%',
-      color: '#fff',
-      textAlign: 'center',
-      lineHeight: '40px',
-      fontSize: '14px',
-      fontWeight: 'bold', 
-      boxShadow: '0 0 6px 4px rgba(93, 162, 113, 0.3)',
-    },
-    {
-      width: '50px',
-      height: '50px',
-      background: 'radial-gradient(circle, rgba(247, 199, 70, 0.9) 40%, rgba(0, 0, 0, 0) 100%', // yellow
-      borderRadius: '50%',
-      color: '#fff',
-      textAlign: 'center',
-      lineHeight: '50px',
-      fontSize: '15px',
-      fontWeight: 'bold',
-      boxShadow: '0 0 10px 6px rgba(254, 228, 64, 0.3)',
-    },
-    {
-      width: '60px',
-      height: '60px',
-      background: 'radial-gradient(circle, rgba(231, 111, 81, 0.8) 40%, rgba(0, 0, 0, 0) 100% )',
-      borderRadius: '50%',
-      color: '#fff',
-      textAlign: 'center',
-      lineHeight: '60px',
-      fontSize: '16px',
-      fontWeight: 'bold',
-      boxShadow: '0 0 10px 6px rgba(231, 111, 81, 0.3)',
-    },
-  ]}
->
+              averageCenter={true}
+              minLevel={4}
+              minClusterSize={3}
+              onCreate={onCreate}
+              calculator={[10, 30, 50, 100]}
+              styles={[
+                {
+                  width: '30px',
+                  height: '30px',
+                  background: 'radial-gradient(circle, rgba(26, 143, 227, 0.8) 40%, rgba(0, 0, 0, 0) 100%', // deep blue
+                  borderRadius: '50%',
+                  color: '#fff',
+                  textAlign: 'center',
+                  lineHeight: '30px',
+                  fontSize: '13px',
+                  fontWeight: 'bold',
+                  boxShadow: '0 0 6px 4px rgba(26, 143, 227, 0.3)',
+                },
+                {
+                  width: '45px',
+                  height: '45px',
+                  background: 'radial-gradient(circle, rgba(93, 162, 113, 0.8) 40%, rgba(0, 0, 0, 0) 100% )', // teal
+                  borderRadius: '50%',
+                  color: '#fff',
+                  textAlign: 'center',
+                  lineHeight: '45px',
+                  fontSize: '14px',
+                  fontWeight: 'bold', 
+                  boxShadow: '0 0 6px 4px rgba(93, 162, 113, 0.3)',
+                },
+                {
+                  width: '60px',
+                  height: '60px',
+                  background: 'radial-gradient(circle, rgba(247, 199, 70, 0.9) 40%, rgba(0, 0, 0, 0) 100%', // yellow
+                  borderRadius: '50%',
+                  color: '#fff',
+                  textAlign: 'center',
+                  lineHeight: '60px',
+                  fontSize: '15px',
+                  fontWeight: 'bold',
+                  boxShadow: '0 0 10px 6px rgba(254, 228, 64, 0.3)',
+                },
+                {
+                  width: '70px',
+                  height: '70px',
+                  background: 'radial-gradient(circle, rgba(231, 111, 81, 0.8) 40%, rgba(0, 0, 0, 0) 100% )',
+                  borderRadius: '50%',
+                  color: '#fff',
+                  textAlign: 'center',
+                  lineHeight: '70px',
+                  fontSize: '16px',
+                  fontWeight: 'bold',
+                  boxShadow: '0 0 10px 6px rgba(231, 111, 81, 0.3)',
+                },
+              ]}
+            >
             {companies.map((company, index) => (
               <MapOverlay
                 key={company.id}
