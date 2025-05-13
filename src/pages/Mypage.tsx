522e8ea8278a9a12ddc4a9337f198d4c2c46ed24
bananackck <emilly03141221@gmail.com>
2025-05-13 13:09:18 +0900

    (Feat) get user name

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -3,6 +3,7 @@ import { useAuthStore } from "@/store/auth";
 import { Button } from "@/components/ui/button";
 import noProfile from '@/assets/no-profile.png'
 import { Modal } from "@/components/ui/modal";
+import { useState, useEffect } from "react";
 
 const logout = async () => {
   const token = useAuthStore.getState().token;
@@ -19,16 +20,41 @@ const logout = async () => {
     window.location.href = "/";
   } catch (error) {
     console.error("로그아웃 실패:", error);
+    
   }
 }
+
+
 export default function Mypage() {
+  const token = useAuthStore((state) => state.token);
+  const [nickname, setNickname] = useState<string>("닉네임");
+
+  useEffect(() => {
+    if (!token) return;
+
+    const fetchUserInfo = async () => {
+      try {
+        const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/v1/members`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setNickname(res.data.data.nickname);
+      } catch (err) {
+        console.error("유저 정보 조회 실패:", err);
+      }
+    };
+
+    fetchUserInfo();
+  }, [token]);
+
   return (
     <>
     <div className="flex flex-col grow">
       <div className="flex h-fit px-8 py-4 gap-4 border border-transparent border-b-border/30">
         <img src={noProfile} className="bg-white rounded-full w-16 h-16 object-fill"></img>
         <div className="flex flex-col my-auto text-text-primary gap-1">
-          <div className="text-xl my-auto font-bold">닉네임</div>
+          <div className="text-xl my-auto font-bold">{nickname}</div>
           <div className="text-sm">포인트 0</div>
         </div>
       </div>
@@ -43,6 +69,7 @@ export default function Mypage() {
                   variant="link"
                   className="mx-16 mb-8"
                 />}
+        // trigger="로그아웃"
         title="로그아웃 하시겠어요?"
         description=  {<>
                         다음에 서비스를 더 편하게 이용하시려면<br />로그인 상태를 유지해 주세요.
