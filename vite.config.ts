c619e65e5e768e0c7e9d846827f79d5c11b29ce5
bananackck <emilly03141221@gmail.com>
2025-05-18 19:35:42 +0900

    (Chore) add mkcert for dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,12 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ['react-icons'],
+  },
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    },
+    port: 5173,
   },
 });
