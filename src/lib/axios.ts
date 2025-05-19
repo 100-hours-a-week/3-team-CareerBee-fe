f51535cfea8ebdde1bfc8d566c72d5bd23b3097f
bananackck <emilly03141221@gmail.com>
2025-05-19 11:28:12 +0900

    (Feat) Error toast for token process

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useAuthStore } from '@/store/auth';
-// import { publishErrorToast } from '@/lib/errorEvents'
+import { publishErrorToast } from '@/lib/errorEvents'
 // import { useToast } from '@/hooks/useToast';
 
 
@@ -17,7 +17,6 @@ export const instance = axios.create({
 instance.interceptors.response.use(
   (response) => response,
   async (error) => {
-    // const { toast } = useToast();
     const res = error.response;
     // console.log(res?.status);
     if (res?.status === 400 || res?.status === 401) {
@@ -30,6 +29,7 @@ instance.interceptors.response.use(
         // 1️⃣ 로그아웃 케이스
         if (message?.includes("로그아웃")) {
           error.code = "LOGGED_OUT";
+          publishErrorToast("로그아웃되었습니다. 다시 로그인 해주세요.");
           // toast({title: "로그아웃되었습니다. 다시 로그인 해주세요."});
           console.log('🔑 로그아웃됨');
           useAuthStore.getState().clearToken();
@@ -64,10 +64,12 @@ instance.interceptors.response.use(
               // 너무 많이 실패하면 로그아웃 처리
               if (!res) {
                 // 네트워크 오류 (서버 응답 없음)
+                publishErrorToast("네트워크 문제입니다. 다시 시도해 주세요.");
                 // toast({title: "네트워크 문제입니다. 다시 시도해 주세요."});
                 return Promise.reject(err);
               } else if (res.status === 401 || res.status === 403) {
                 // 재로그인 유도
+                publishErrorToast("다시 로그인해주세요 ㅠㅜ");
                 // toast({title: "다시 로그인해주세요 ㅠㅜ"});
                 useAuthStore.getState().clearToken();
                 window.location.href = "/login";
