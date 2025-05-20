58ff3b9554679825b8b523d6aa9fd369831c95e5
bananackck <emilly03141221@gmail.com>
2025-05-20 14:24:54 +0900

    (Chore) dependencies for checking bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    //번들 크기 측정
+    visualizer({
+      open: true,
+      filename: 'bundle-report.html',
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
