af19f4580a01213c86d11a3da6bdcbc4d611cdf0
bananackck <emilly03141221@gmail.com>
2025-05-23 15:20:47 +0900

    (Feat) change the way of adding background image for chart bars for better animation

--- src/features/Competition/chart.tsx
+++ src/features/Competition/chart.tsx
@@ -23,76 +23,38 @@ export default function BarChart() {
     const svg = d3
       .select(svgRef.current)
       .attr('viewBox', `0,0,${width},${height}`)
-      .attr("fill-opacity", 0.6)
       .attr("width", width)
       .attr("height", height);
-    // svg.selectAll("*").remove();
 
     const defs = svg.append("defs");
-    
-    const setBackground = (
-      defs: d3.Selection<SVGDefsElement, unknown, null, undefined>,
-      data: chartProps[]
-    )=>{
-      defs.selectAll("*").remove();
-      const dataLength=mock.length;
-      data.forEach((data, i) => {
-        const color = data.rank <= 1 ? 'red' : data.rank <= 2 ? 'green' : data.rank <= 3 ? 'blue' : 'yellow';
-        defs.append("pattern")
-          .attr("id", `pattern-${i}`)
-          .attr("patternUnits", "userSpaceOnUse")
-          .attr("width", width)
-          .attr("height", barHeight+gap)
-          .attr("x", 0) // Ensure pattern x is explicitly set to 0
-          .append("image")
-          .attr("href", `/assets/${color}-rank.svg`)
-          .attr("width", width)
-          .attr("height", barHeight)
-          .attr("preserveAspectRatio", "none")
-          // .attr("x", width)
-            .attr("x", xScale(data.value))
 
-          .attr('y', (data.rank-1)*barHeight+(data.rank-1)*gap)
-
-          // .attr('y', (dataLength-1)*barHeight+(dataLength-1)*gap)
-          // .transition()
-          //   .duration(transTime)
-          //   // .attr("x", y(data.value))
-          //   .attr("y", 0);
-      });
-    }
-    
     const bars = (svg: d3.Selection<SVGSVGElement | null, unknown, null, undefined>)=>{
       let bar = svg
         .append("g")
         .selectAll<SVGRectElement, chartProps>("rect")
-        
         
         return (data: chartProps[]) => {
           const dataLength=data.length;
           const widthScale = d3.scaleLinear()
             .domain([0, d3.max(data, d => d.value)!])
             .range([0, width]);
-        // setBackground(defs);   //🖌️배경 적용 시 크기
         bar = bar.data(data)
           .join(
-            (enter) => enter
-              .append("rect")
-              // .attr('fill', (_, i) => `url(#pattern-${i})`)
-              .attr('fill',  'red')
+            (enter) => enter.append("rect")
+              .attr("id", d => `bar-${d.rank}`)
+              .attr('fill', 'transparent')
               .attr('y', (data)=>(data.rank-1)*barHeight+(data.rank-1)*gap)
               .attr('x', width)
-              .attr("width", 0)
+              .attr("width", width)
               .attr('height', barHeight)
               .attr('rx', 8)
               .attr('ry', 8)
+              .attr("id", d => `bar-${d.nickname}`)
               .transition()
                 .duration(transTime)
                 .attr('x', (data) => xScale(data.value))
-                .attr('width', (data) => widthScale(data.value))
               ,
             (update) => update
-              .attr('fill', 'blue')
               .attr('x', (data) => {
                 const prevItem = prev.find(p => p.nickname === data.nickname);
                 const prevValue = prevItem ? prevItem.value : 0;
@@ -107,7 +69,7 @@ export default function BarChart() {
                   const prevItem = prev.find(p => p.nickname === data.nickname);
                   const prevRank = prevItem ? prevItem.value : 0;
                   return widthScale(prevRank)
-                  })
+              })
               .transition()
                 .duration(transTime)
                 .attr('y', (data) => (data.rank-1)*barHeight+(data.rank-1)*gap)
@@ -121,15 +83,76 @@ export default function BarChart() {
               .attr('x', width)
               .attr("width", 0)
           )
+          data.forEach(d => {
+            defs.append("clipPath")
+              .attr("id", `clip-bar-${d.nickname}`)
+              .append("use")
+              .attr("xlink:href", `#bar-${d.nickname}`);
+          });
         }
+    }
+
+    function labels(svg: d3.Selection<SVGSVGElement | null, unknown, null, undefined>) {
+      let label = svg.append("g")
+          .style("font", "bold 12px var(--font-pretendard)")
+          .attr("text-anchor", "end")
+          .selectAll<SVGImageElement, chartProps>("image");
+
+      return (data: chartProps[]) => {
+        const dataLength=data.length;
+        label = label.data(data)
+        .join(
+          (enter) => enter.append("image")
+            .attr("preserveAspectRatio", "none")
+            .attr("clip-path", d => `url(#clip-bar-${d.nickname})`)
+            .attr('y', (data)=>(data.rank-1)*barHeight+(data.rank-1)*gap)
+            .attr('x', width)
+            .attr("width", width)
+            .attr('height', barHeight)
+            .attr("href", d => {
+              if (d.rank === 1) return "/assets/red-rank.svg";
+              if (d.rank === 2) return "/assets/green-rank.svg";
+              if (d.rank === 3) return "/assets/blue-rank.svg";
+              return "/assets/yellow-rank.svg"; // rank ≥ 4
+            })
+            .transition()
+              .duration(transTime)
+              .attr('x', (data) => xScale(data.value)),
+          update => update
+            .attr("preserveAspectRatio", "none")
+            .attr('x', (data) => {
+              const prevItem = prev.find(p => p.nickname === data.nickname);
+              const prevValue = prevItem ? prevItem.value : 0;
+              return xScale(prevValue);
+            })
+            .attr('y', (data) => {
+              const prevItem = prev.find(p => p.nickname === data.nickname);
+              const prevRank = prevItem ? prevItem.rank : dataLength;
+              return (prevRank - 1) * barHeight + (prevRank - 1) * gap;
+            })
+            .transition()
+              .duration(transTime)
+              .attr('y', (data) => (data.rank-1)*barHeight+(data.rank-1)*gap)
+              .attr('x', (data) => xScale(data.value))
+            ,
+          (exit) => exit.transition()
+              .duration(transTime)
+              .ease(d3.easeLinear)
+              .remove()
+              .attr('x', width)
+              .attr("width", 0)
+        )
       }
-
-    const bar = bars(svg);
-    bar(mock[0]);
-
+  }
+    const updateBars = bars(svg);
+    const updateLabels = labels(svg);
+    updateBars(mock[0]);
+    updateLabels(mock[0]);
     prev = mock[0]
     setTimeout(() => {
-        bar(mock[1]);
+      updateBars(mock[1]);
+      updateLabels(mock[1]);
+
     }, 5000);
 
   }, []);
