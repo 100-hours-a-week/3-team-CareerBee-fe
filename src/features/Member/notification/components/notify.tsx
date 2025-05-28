80fa88235036907c4dcb503c51ecd37af4704159
bananackck <emilly03141221@gmail.com>
2025-05-28 10:38:53 +0900

    (Feat) add icons for notify

--- src/features/Member/notification/components/notify.tsx
+++ src/features/Member/notification/components/notify.tsx
@@ -1,10 +1,23 @@
 import { cn } from '@/lib/utils';
+import competition from '@/features/Member/notification/image/competition.png';
+import growth from '@/features/Member/notification/image/growth.png';
+import point from '@/features/Member/notification/image/point.png';
+import recruite from '@/features/Member/notification/image/recruite.png';
 
 interface NotifyProps {
   title: string;
   description: string;
   time: string;
 }
+
+const iconMap: Record<string, string> = {
+  '공채 알림': recruite,
+  'CS 대회': competition,
+  // eslint-disable-next-line prettier/prettier
+  '진척도': growth,
+  // eslint-disable-next-line prettier/prettier
+  '포인트': point,
+};
 
 export default function Notify({ title, description, time }: NotifyProps) {
   return (
@@ -15,9 +28,13 @@ export default function Notify({ title, description, time }: NotifyProps) {
       )}
     >
       <div className="flex justify-between w-full text-xs">
-        <>
+        <div className="flex align-bottom gap-1">
+          {(() => {
+            const imageSrc = iconMap[title] || undefined;
+            return <img src={imageSrc} alt={title} className="w-4 h-4" />;
+          })()}
           <div className="flex mr-auto">{title}</div>
-        </>
+        </div>
         <div className="flex text-left text-text-secondary">{time}</div>
       </div>
       <div className="w-full text-sm">{description}</div>
