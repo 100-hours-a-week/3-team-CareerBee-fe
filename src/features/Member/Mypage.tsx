d0a9325f81c146cae71ceca7c72ca110baec06d6
bananackck <emilly03141221@gmail.com>
2025-05-30 04:29:15 +0900

    (Feat) add fallback ui to saved companies

--- src/features/Member/Mypage.tsx
+++ src/features/Member/Mypage.tsx
@@ -36,6 +36,8 @@ export default function Mypage() {
     fetchUserInfo();
   }, [token]);
 
+  const companies = [...Array(0)];
+
   return (
     <>
       <div className="flex flex-col grow">
@@ -74,8 +76,8 @@ export default function Mypage() {
         <div className="flex flex-col w-full items-center justify-center px-6 py-3 gap-2 border border-transparent border-b-border/30">
           <div className="text-base font-bold w-full items-start">관심 기업</div>
           <div className="flex items-start justify-start w-full overflow-x-auto gap-2 pb-1">
-            {[...Array(3)].map(() => {
-              return (
+            {companies && companies.length > 0 ? (
+              companies.map(() => (
                 <CompanyCard
                   companyId={1}
                   companyName="샘플 기업"
@@ -86,8 +88,12 @@ export default function Mypage() {
                   isLoggedIn={!!token}
                   setIsBookmarked={setIsBookmarked}
                 />
-              );
-            })}
+              ))
+            ) : (
+              <div className="flex justify-center text-sm text-text-secondary py-2 w-full">
+                지도에서 관심 기업을 추가해보세요!
+              </div>
+            )}
           </div>
         </div>
       </div>
