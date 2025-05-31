908b2139de6b04ec6436a48568cd5c6bfb2a084e
bananackck <emilly03141221@gmail.com>
2025-06-01 04:06:09 +0900

    (Feat) add router to resume/upload

---
+++ src/features/Member/resume/UploadResume.tsx
@@ -0,0 +1,9 @@
+export default function UploadResume() {
+  return (
+    <div className="py-3 w-full mb-auto">
+      <div className="flex flex-col px-16 pb-3 gap-2 border border-transparent border-b-border/30">
+        <div className="text-base font-bold w-full items-start">회원 정보 관리</div>
+      </div>
+    </div>
+  );
+}
