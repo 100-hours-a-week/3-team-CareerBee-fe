c280e33d6c71de7eff4fcd9da0e274fb52a2baa4
bananackck <emilly03141221@gmail.com>
2025-06-01 20:56:38 +0900

    (Feat) make inputs and textarea to form

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -17,6 +17,17 @@ const SearchVariants = cva(`flex w-full placeholder:text-text-secondary text-bas
         disabled:cursor-not-allowed 
         disabled:opacity-50 
         md:text-sm`,
+      resume: `flex h-10 w-full items-center justify-between whitespace-nowrap 
+        rounded-lg border border-border 
+        bg-transparent px-3 py-2 text-sm font-medium text-text-primary shadow-sm 
+        ring-offset-background 
+        data-[placeholder]:text-text-primary 
+        focus:outline-none 
+        focus:ring-1
+        focus:ring-ring 
+        disabled:cursor-not-allowed 
+        disabled:opacity-50 
+        [&>span]:line-clamp-1 leading-5`,
 
       search: `h-11 rounded-full pl-10 pr-10 focus-visible:outline-none `,
     },
@@ -28,7 +39,7 @@ const SearchVariants = cva(`flex w-full placeholder:text-text-secondary text-bas
 
 const Input = React.forwardRef<
   HTMLInputElement,
-  React.ComponentProps<'input'> & { variant?: 'default' | 'search' }
+  React.ComponentProps<'input'> & { variant?: 'default' | 'search' | 'resume' }
 >(({ className, type, maxLength, variant = 'default', ...props }, ref) => {
   return (
     <input
--- src/features/Member/resume/UploadResume.tsx
+++ src/features/Member/resume/UploadResume.tsx
@@ -1,9 +1,134 @@
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textbox';
+import Dropdown from '@/components/ui/dropdown';
+
 export default function UploadResume() {
   return (
-    <div className="py-3 w-full mb-auto">
-      <div className="flex flex-col px-16 pb-3 gap-2 border border-transparent border-b-border/30">
-        <div className="text-base font-bold w-full items-start">회원 정보 관리</div>
+    <div className="flex flex-col py-3 px-16 w-full mb-auto gap-4">
+      <div className="flex flex-col">
+        <div className="text-base font-bold w-full items-start">
+          진척도 조회를 위해 정보를 입력해주세요.
+        </div>
+        <p className="text-xs text-text-secondary">해당 정보는 참고용입니다.</p>
       </div>
+      <form>
+        <div className="flex flex-col gap-2 w-full">
+          {/* 선호 직무 */}
+          <div className="flex flex-col w-full gap-1">
+            <p className="text-sm font-medium">선호 직무*</p>
+            <Dropdown
+              placeholder="선호 직무"
+              items={[
+                { label: '프론트엔드', value: 'FE' },
+                { label: '백엔드', value: 'BE' },
+                { label: 'AI', value: 'AI' },
+                { label: '클라우드(DevOps)', value: 'CLOUD' },
+              ]}
+              // onChange={(value) => setSelectedReason(value)}
+            />
+          </div>
+
+          {/* 백준 티어 */}
+          <div className="flex flex-col w-full gap-1">
+            <p className="text-sm font-medium">백준 티어*</p>
+            <Dropdown
+              placeholder="백준 티어"
+              items={[
+                { label: '프론트엔드', value: 'FE' },
+                { label: '백엔드', value: 'BE' },
+                { label: 'AI', value: 'AI' },
+                { label: '클라우드(DevOps)', value: 'CLOUD' },
+              ]}
+              // onChange={(value) => setSelectedReason(value)}
+            />
+          </div>
+
+          {/* IT 자격증 개수 */}
+          <div className="flex flex-col w-full gap-1">
+            <div className="text-sm flex w-full">
+              <p className=" font-medium mr-auto">IT 자격증 개수*</p>
+              <p title="helper-text" className="font-medium text-error">
+                *helper text
+              </p>
+            </div>
+            <Input
+              variant="resume"
+              placeholder="숫자를 입력해주세요."
+              onChange={(e) => {
+                // setNickname(e.target.value);
+              }}
+            />
+          </div>
+
+          {/* 프로젝트 개수 */}
+          <div className="flex flex-col w-full gap-1">
+            <div className="text-sm flex w-full">
+              <p className=" font-medium mr-auto">프로젝트 개수*</p>
+              <p title="helper-text" className="font-medium text-error">
+                *helper text
+              </p>
+            </div>
+            <Input
+              variant="resume"
+              placeholder="이력서에 추가할 프로젝트 개수를 입력해주세요."
+              onChange={(e) => {
+                // setNickname(e.target.value);
+              }}
+            />
+          </div>
+
+          {/* 근무 기간 */}
+          <div className="flex flex-col w-full gap-1">
+            <div className="text-sm flex w-full">
+              <p className=" font-medium mr-auto">근무 기간</p>
+              <p title="helper-text" className="font-medium text-error">
+                *helper text
+              </p>
+            </div>
+            <Input
+              variant="resume"
+              placeholder="월 단위로 입력해주세요."
+              onChange={(e) => {
+                // setNickname(e.target.value);
+              }}
+            />
+          </div>
+
+          {/* 직무 */}
+          <div className="flex flex-col w-full gap-1">
+            <div className="text-sm flex w-full">
+              <p className=" font-medium mr-auto">직무</p>
+              <p title="helper-text" className="font-medium text-error">
+                *helper text
+              </p>
+            </div>
+            <Input
+              variant="resume"
+              placeholder="담당 직무를 입력해주세요."
+              onChange={(e) => {
+                // setNickname(e.target.value);
+              }}
+            />
+          </div>
+
+          {/* 기타 어필 */}
+          <div className="flex flex-col w-full gap-1">
+            <div className="text-sm flex w-full">
+              <p className=" font-medium mr-auto">직무</p>
+              <p title="helper-text" className="font-medium text-error">
+                *helper text
+              </p>
+            </div>
+            <Textarea
+              maxLength={100}
+              placeholder="TOPCIT, 수상이력, 기술 스터디, 대회 참가 이력..."
+              onChange={(e) => {
+                // setNickname(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
