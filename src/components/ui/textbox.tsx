c1092ccde1a51af8e43fed52be67b41d445a1ad3
bananackck <emilly03141221@gmail.com>
2025-06-01 20:56:08 +0900

    (Feat) show count inside textbox

--- src/components/ui/textbox.tsx
+++ src/components/ui/textbox.tsx
@@ -1,20 +1,35 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 import { cn } from '@/lib/utils';
 import { cva } from 'class-variance-authority';
 
-const variants = cva(`flex w-full placeholder:text-text-secondary text-base`, {
+const divVariants = cva(``, {
   variants: {
     variant: {
-      default: `flex h-40 w-full items-center justify-between
+      default: `flex flex-col h-40 w-full items-center justify-between
         rounded-lg border border-border shadow-sm bg-transparent
-        px-3 py-2 overflow-y-auto resize-none	
-        text-sm font-medium text-text-primary
+        px-3 py-2
         ring-offset-background 
+        focus-within:border-ring
+        focus-within:ring-1
+        focus-within:ring-ring 
+        disabled:cursor-not-allowed 
+        disabled:opacity-50 `,
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+const textVariants = cva(`flex w-full placeholder:text-text-secondary text-base`, {
+  variants: {
+    variant: {
+      default: `flex w-full h-full items-center justify-between
+        overflow-y-auto resize-none	bg-transparent
+        text-sm font-medium text-text-primary
         data-[placeholder]:text-text-primary 
         focus:outline-none 
-        focus:ring-1
-        focus:ring-ring 
         disabled:cursor-not-allowed 
         disabled:opacity-50 
         [&>span]:line-clamp-1 leading-5`,
@@ -26,15 +41,33 @@ const variants = cva(`flex w-full placeholder:text-text-secondary text-base`, {
 });
 
 const Textarea = React.forwardRef<
-  HTMLInputElement,
-  React.ComponentProps<'textarea'> & { variant?: 'default' }
->(({ className, maxLength, variant = 'default', ...props }, ref) => {
+  HTMLTextAreaElement,
+  React.ComponentProps<'textarea'> & { variant?: 'default'; showCount?: boolean }
+>(({ className, maxLength, variant = 'default', onChange, showCount = true, ...props }, ref) => {
+  const [textCount, setTextCount] = useState('000');
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextCount(
+      String(e.target.value.length).padStart(maxLength ? String(maxLength).length : 3, '0'),
+    );
+    onChange?.(e);
+  };
+
   return (
-    <textarea
-      className={cn(variants({ variant }), className)}
-      maxLength={maxLength}
-      {...props}
-    />
+    <div className={cn(divVariants({ variant }), className)}>
+      <textarea
+        className={cn(textVariants({ variant }))}
+        maxLength={maxLength}
+        ref={ref}
+        onChange={handleChange}
+        {...props}
+      />
+      {showCount && (
+        <p className="w-full text-end text-xs text-text-secondary">
+          <span>{textCount}</span>
+          <span>/{maxLength}</span>
+        </p>
+      )}
+    </div>
   );
 });
 Textarea.displayName = 'Textarea';
