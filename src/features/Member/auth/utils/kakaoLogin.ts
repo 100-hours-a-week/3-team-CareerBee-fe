5c2eca5b6d51245ca7518c54be88efe1d4a0e9cc
bananackck <emilly03141221@gmail.com>
2025-06-03 22:01:41 +0900

    (Refact) use public request at mypage.tsx and kakaoLogin.ts

--- src/features/Member/auth/utils/kakaoLogin.ts
+++ src/features/Member/auth/utils/kakaoLogin.ts
@@ -1,17 +1,14 @@
-import { instance as axios } from '@/features/Member/auth/utils/axios';
+import { safeGet } from '@/lib/request';
 
 const handleLogin = async () => {
-  try {
-    const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/v1/auth/oauth`, {
-      params: { type: 'KAKAO' },
-    });
+  const res = await safeGet('/api/v1/auth/oauth', {
+    params: { type: 'KAKAO' },
+  });
+  if (res) {
     const loginUrl = res.data.data.loginUrl;
     if (loginUrl) {
       window.location.href = loginUrl;
     }
-  } catch (error) {
-    console.error('로그인 URL 요청 실패:', error);
-    alert('로그인에 실패했습니다. 다시 시도해주세요.');
   }
 };
 
