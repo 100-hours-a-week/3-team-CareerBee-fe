8f487ae700735492ee3c58c5c77c678fbbd82d2e
bananackck <emilly03141221@gmail.com>
2025-06-04 13:07:57 +0900

    (Refact) update user info in mypage using query

--- src/lib/getUserInfo.ts
+++ src/lib/getUserInfo.ts
@@ -1,4 +1,3 @@
-// src/api/user.ts
 import { safeGet } from '@/lib/request';
 
 export const getUserInfo = async (token: string) => {
@@ -8,5 +7,5 @@ export const getUserInfo = async (token: string) => {
     },
   });
 
-  return res.data.data; // { nickname, point, profileImage, badgeImage, frameImage }
+  return res.data.data; // { nickname, hasNewNotification, point, profileImage, badgeImage, frameImage }
 };
