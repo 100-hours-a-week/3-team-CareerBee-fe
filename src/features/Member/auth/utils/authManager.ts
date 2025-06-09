a40536847e04545554d1e6e91a088f7d51cecd07
bananackck <emilly03141221@gmail.com>
2025-06-09 23:17:27 +0900

    (Feat) retry reissue max 2 times on reissue canceled and 500+.

--- src/features/Member/auth/utils/authManager.ts
+++ src/features/Member/auth/utils/authManager.ts
@@ -4,26 +4,44 @@ import { useCompetitionStore } from '@/features/Competition/store/competitionSto
 import { instance as axios } from '@/features/Member/auth/utils/axios';
 import { toast } from '@/hooks/useToast';
 import { queryClient } from '@/lib/react-query-client';
-import originAxios from 'axios';
+import originAxios, { AxiosError } from 'axios';
 
 export async function retryWithRefreshedToken(config: AxiosRequestConfig) {
-  try {
-    const res = await axios.post('/api/v1/auth/reissue', null, {
-      withCredentials: true,
-    });
-    const newToken = res.data.data.newAccessToken;
-    useAuthStore.getState().setToken(newToken);
-    config.headers = {
-      ...config.headers,
-      Authorization: `Bearer ${newToken}`,
-    };
-    return originAxios(config);
-  } catch (e) {
-    if (!originAxios.isCancel(e)) {
-      forceLogout();
+  const MAX_RETRIES = 2;
+
+  for (let attempt = 1; attempt <= MAX_RETRIES; attempt++) {
+    try {
+      const res = await axios.post('/api/v1/auth/reissue', null, {
+        withCredentials: true,
+      });
+      const newToken = res.data.data.newAccessToken;
+      useAuthStore.getState().setToken(newToken);
+      config.headers = {
+        ...config.headers,
+        Authorization: `Bearer ${newToken}`,
+      };
+      return originAxios(config);
+    } catch (e: unknown) {
+      if (originAxios.isCancel(e)) {
+        continue;
+      }
+
+      const status = (e as AxiosError)?.response?.status;
+      if ((status && status < 500) || (!originAxios.isCancel(e) && attempt >= 1)) {
+        forceLogout();
+        return Promise.reject(e);
+      }
+
+      // 재시도
+      if (attempt < MAX_RETRIES) {
+        await new Promise((r) => setTimeout(r, 1000));
+      } else {
+        return Promise.reject(e);
+      }
     }
-    return Promise.reject(e);
   }
+
+  return Promise.reject(new Error('Maximum reissue attempts reached'));
 }
 export function forceLogout() {
   toast({ title: '로그아웃 되었습니다.' });
