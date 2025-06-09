5c1e882b0d5377025b17f11077e546ffa1d4e627
Bukyung Kim <76582861+bananackck@users.noreply.github.com>
2025-06-09 20:24:59 +0900

    [Member] infinity scroll to wish company list (#205)
    * (Feat) implement infinite scroll to wish company list
    
    * (Feat) add end of list component
    
    * (Feat) make circular loading spinner
    
    * (Feat) add loading spinner on data fetching
    
    * (Fix) fix data response typo&align loading spinner to center
    
    * (Feat) give next cursor as a param for infinite scrolling

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -1,9 +1,18 @@
 import '@/static/loader.css';
+import '@/static/circleLoader.css';
 
 export const Loader = () => {
   return (
     <div className="flex items-center justify-center">
       <div className={`loader`} />
+    </div>
+  );
+};
+
+export const CircleLoader = ({ size = 50 }: { size?: number }) => {
+  return (
+    <div className="flex items-center justify-center" style={{ width: size, height: size }}>
+      <div className="circleLoader" />
     </div>
   );
 };
