569e4857ee3a18eb5f5b53127e35951d221974c2
bananackck <emilly03141221@gmail.com>
2025-06-11 09:43:35 +0900

    (Fix) use safePost for  posting competition join data

--- src/features/Competition/hooks/useCompetitionData.ts
+++ src/features/Competition/hooks/useCompetitionData.ts
@@ -1,13 +1,11 @@
 import { useEffect, useState } from 'react';
 import { Problem } from '../Competition';
-import { safeGet } from '@/lib/request';
-// import { useCompetitionStore } from '@/features/Competition/store/competitionStore';
+import { safeGet, safePost } from '@/lib/request';
 import { useAuthStore } from '@/features/Member/auth/store/auth';
 
 export function useCompetitionData(competitionId: number | null) {
   const [problems, setProblems] = useState<Problem[]>([]);
   const token = useAuthStore((state) => state.token);
-  // const { competitionId } = useCompetitionStore();
 
   useEffect(() => {
     const fetchProblems = async () => {
@@ -26,11 +24,10 @@ export function useCompetitionData(competitionId: number | null) {
     };
 
     const joinCompetition = async () => {
-      const res = await fetch(`/api/v1/competitions/${competitionId}`, {
-        method: 'POST',
+      const res = await safePost(`/api/v1/competitions/${competitionId}`, {
         headers: { Authorization: `Bearer ${token}` },
       });
-      if (res.status !== 204) {
+      if (res.httpStatusCode !== 204) {
         alert('대회 참가에 실패했습니다.');
       }
     };
