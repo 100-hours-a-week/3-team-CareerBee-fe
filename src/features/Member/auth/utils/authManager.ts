0b5dd0741bac31ab7d3cd38f5ad90db3a804af2e
bananackck <emilly03141221@gmail.com>
2025-06-12 13:44:27 +0900

    (Fix) do not retry on reissue fail

--- src/features/Member/auth/utils/authManager.ts
+++ src/features/Member/auth/utils/authManager.ts
@@ -6,42 +6,30 @@ import { queryClient } from '@/lib/react-query-client';
 import originAxios, { AxiosError } from 'axios';
 
 export async function retryWithRefreshedToken(config: AxiosRequestConfig) {
-  const MAX_RETRIES = 2;
-
-  for (let attempt = 1; attempt <= MAX_RETRIES; attempt++) {
-    try {
-      const res = await originAxios.post(
-        `${import.meta.env.VITE_API_URL}/api/v1/auth/reissue`,
-        null,
-        {
-          withCredentials: true,
-        },
-      );
-      const newToken = res.data.data.newAccessToken;
-      useAuthStore.getState().setToken(newToken);
-      config.headers = {
-        ...config.headers,
-        Authorization: `Bearer ${newToken}`,
-      };
-      return originAxios(config);
-    } catch (e: unknown) {
-      if (originAxios.isCancel(e)) {
-        continue;
-      }
-
-      const status = (e as AxiosError)?.response?.status;
-      if ((status && status < 500) || (!originAxios.isCancel(e) && attempt >= 1)) {
-        await forceLogout();
-        return Promise.reject(e);
-      } else if (attempt < MAX_RETRIES) {
-        await new Promise((r) => setTimeout(r, 1000));
-      } else {
-        return Promise.reject(e);
-      }
+  try {
+    const res = await originAxios.post(
+      `${import.meta.env.VITE_API_URL}/api/v1/auth/reissue`,
+      null,
+      {
+        withCredentials: true,
+      },
+    );
+    const newToken = res.data.data.newAccessToken;
+    useAuthStore.getState().setToken(newToken);
+    config.headers = {
+      ...config.headers,
+      Authorization: `Bearer ${newToken}`,
+    };
+    return originAxios(config);
+  } catch (e: unknown) {
+    const status = (e as AxiosError)?.response?.status;
+    if (status && status < 500 && !originAxios.isCancel(e)) {
+      await forceLogout();
+      return Promise.reject(e);
+    } else {
+      return Promise.reject(e);
     }
   }
-
-  return Promise.reject(new Error('Maximum reissue attempts reached'));
 }
 
 export async function forceLogout() {
