ebed32cee5162c4113c55e69e76da399937938ea
bananackck <emilly03141221@gmail.com>
2025-06-12 21:31:16 +0900

    (Feat) include authrization token to sse header

--- src/features/Member/notification/hooks/useNotificationSSE.ts
+++ src/features/Member/notification/hooks/useNotificationSSE.ts
@@ -1,15 +1,20 @@
+import { useAuthStore } from '../../auth/store/auth';
+
 import { useEffect } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
-import { EventSourcePolyfill } from 'event-source-polyfill';
+import { EventSourcePolyfill, NativeEventSource } from 'event-source-polyfill';
 
 export const useNotificationSSE = (enabled: boolean) => {
   const queryClient = useQueryClient();
-
+  const token = useAuthStore.getState().token;
   useEffect(() => {
     if (!enabled) return;
-
-    const EventSource = EventSourcePolyfill;
-    const eventSource = new EventSource(`${import.meta.env.VITE_API_URL}/api/v1/sse/subscribe`);
+    const EventSource = EventSourcePolyfill || NativeEventSource;
+    const eventSource = new EventSource(`${import.meta.env.VITE_API_URL}/api/v1/sse/subscribe`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
     eventSource.onmessage = (event) => {
       const { hasNew } = JSON.parse(event.data); //TODO: response 형식과 맞추기
