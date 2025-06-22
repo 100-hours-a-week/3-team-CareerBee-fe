8ad18380fe084e342d2823b07cbc74257f35ea4e
bananackck <emilly03141221@gmail.com>
2025-06-22 17:10:15 +0900

    (Refact) cleaner code

--- src/features/Competition/hooks/useCompetitionData.ts
+++ src/features/Competition/hooks/useCompetitionData.ts
@@ -1,43 +1,48 @@
-import { useEffect, useState } from 'react';
 import { Problem } from '../Competition';
-import { safeGet, safePost } from '@/lib/request';
+
 import { useAuthStore } from '@/features/Member/auth/store/auth';
 import { toast } from '@/hooks/useToast';
+import { safeGet, safePost } from '@/lib/request';
+
+import { useEffect, useState } from 'react';
 
 export function useCompetitionData(competitionId: number | null) {
   const [problems, setProblems] = useState<Problem[]>([]);
   const token = useAuthStore((state) => state.token);
 
-  useEffect(() => {
-    const fetchProblems = async () => {
-      if (import.meta.env.VITE_USE_MOCK === 'true') {
-        const mock = await fetch('/mock/mock-problems.json');
-        const res = await mock.json();
+  const fetchProblems = async () => {
+    if (import.meta.env.VITE_USE_MOCK === 'true') {
+      const mock = await fetch('/mock/mock-problems.json');
+      const res = await mock.json();
+      setProblems(res.data.problems);
+    } else {
+      const res = await safeGet(`/api/v1/competitions/${competitionId}/problems`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (res.httpStatusCode === 200) {
         setProblems(res.data.problems);
-      } else {
-        const res = await safeGet(`/api/v1/competitions/${competitionId}/problems`, {
-          headers: { Authorization: `Bearer ${token}` },
-        });
-        if (res.httpStatusCode === 200) {
-          setProblems(res.data.problems);
-        }
       }
-    };
+    }
+  };
 
-    const joinCompetition = async () => {
-      const res = await safePost(
+  const joinCompetition = async () => {
+    try {
+      await safePost(
         `/api/v1/competitions/${competitionId}`,
         {},
         {
           headers: { Authorization: `Bearer ${token}` },
         },
       );
-      if (res.httpStatusCode === 409) {
+    } catch (error: any) {
+      if (error.response?.data?.httpStatusCode === 409) {
         toast({ title: '이미 참가한 대회입니다.', variant: 'destructive' });
         window.location.href = '/competition';
       }
-    };
+    }
+  };
 
+  useEffect(() => {
     fetchProblems();
     joinCompetition();
   }, []);
