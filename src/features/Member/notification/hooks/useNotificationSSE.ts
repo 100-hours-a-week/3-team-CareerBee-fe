028f53f74d58943ae0765f1e082d0409a788443a
bananackck <emilly03141221@gmail.com>
2025-06-25 16:19:37 +0900

    (Feat) reconnect SSE on connection failed

--- src/features/Member/notification/hooks/useNotificationSSE.ts
+++ src/features/Member/notification/hooks/useNotificationSSE.ts
@@ -9,49 +9,61 @@ export const useNotificationSSE = () => {
   const queryClient = useQueryClient();
   const sseRef = useRef<EventSource | null>(null);
 
-  // useEffect(() => {
   const token = useAuthStore.getState().token;
   if (sseRef.current) return; // 이미 연결된 경우 실행 방지
   if (!token) return;
 
   const EventSource = EventSourcePolyfill || NativeEventSource;
-  const eventSource = new EventSource(`${import.meta.env.VITE_API_URL}/api/v1/sse/subscribe`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    heartbeatTimeout: 60 * 60 * 1000,
-    withCredentials: true,
-  });
+  let retryCount = 0;
 
-  sseRef.current = eventSource;
+  const connect = () => {
+    const eventSource = new EventSource(`${import.meta.env.VITE_API_URL}/api/v1/sse/subscribe`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      heartbeatTimeout: 60 * 60 * 1000,
+      withCredentials: true,
+    });
 
-  eventSource.addEventListener('open', () => {
-    console.log('SSE 연결 성공', eventSource);
-  });
+    sseRef.current = eventSource;
 
-  eventSource.addEventListener('notification', (e: any) => {
-    if (e.data === 'true') {
-      queryClient.setQueryData(['userInfo'], (prev: any) => ({
-        ...prev,
-        hasNewAlarm: true,
-      }));
-    }
-  });
+    eventSource.addEventListener('open', () => {
+      retryCount = 0;
+    });
 
-  eventSource.onerror = (error) => {
-    console.error('SSE 연결 오류:', error);
-    eventSource.close();
-    sseRef.current = null;
-    if ((error as any).status === 401) {
-      //재연결 요청하기
-      retryWithRefreshedToken(eventSource);
-    }
+    eventSource.addEventListener('notification', (e: any) => {
+      if (e.data === 'true') {
+        queryClient.setQueryData(['userInfo'], (prev: any) => ({
+          ...prev,
+          hasNewAlarm: true,
+        }));
+      }
+    });
+
+    eventSource.onerror = (error) => {
+      eventSource.close();
+      sseRef.current = null;
+
+      // 토큰 이슈로 연결 해제 시
+      if ((error as any).status === 401) {
+        retryWithRefreshedToken(eventSource);
+      } else {
+        const retryDelay = Math.min(1000 * 2 ** retryCount, 30000); // 최대 30초
+        retryCount += 1;
+
+        setTimeout(() => {
+          console.log(`SSE 재연결 시도 (${retryCount}회)...`);
+          connect();
+        }, retryDelay);
+      }
+    };
+
+    return () => {
+      console.log('SSE 연결 해제');
+      eventSource.close();
+      sseRef.current = null;
+    };
   };
 
-  return () => {
-    console.log('SSE 연결 해제');
-    eventSource.close();
-    sseRef.current = null;
-  };
-  // }, []);
+  connect();
 };
