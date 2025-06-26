3538fb58b69a0e3b860a84045a0d6da38c970bb8
bananackck <emilly03141221@gmail.com>
2025-06-26 22:18:44 +0900

    (Feat) pass navigation to uploadPdf function in form submission

--- src/features/Member/resume/upload.tsx
+++ src/features/Member/resume/upload.tsx
@@ -45,7 +45,7 @@ export default function Upload() {
             <p>이력서를 업로드하면, 더 정밀한 분석 결과를 받아볼 수 있어요.</p>
           </div>
         </div>
-        <form onSubmit={(e) => uploadPdf(e, token)}>
+        <form onSubmit={(e) => uploadPdf(e, token, navigate)}>
           <div className="flex flex-col w-full items-center gap-2">
             <label
               htmlFor="resume-upload"
