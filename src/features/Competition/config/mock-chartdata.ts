e62017a1a832d561c9b1b49de88b38633b7f05a2
bananackck <emilly03141221@gmail.com>
2025-06-30 01:01:29 +0900

    (Chore) update mock data

--- src/features/Competition/config/mock-chartdata.ts
+++ src/features/Competition/config/mock-chartdata.ts
@@ -1,102 +1,80 @@
-export interface chartProps {
+export interface ChartProps {
   rank: number;
   nickname: string;
-  profileImgUrl: string;
-  badgeImgUrl: string;
+  profileUrl: string;
   elapsedTime: string;
   solvedCount: number;
-  value: number;
 }
 
-export const mockChart: chartProps[] = [
+export const mockChart: ChartProps[] = [
   {
     rank: 1,
     nickname: '김춘식1',
-    profileImgUrl: '/assets/no-profile.png',
-    badgeImgUrl: '/assets/default.svg',
-    elapsedTime: '03.24.123',
+    profileUrl: 'https://test1.jpeg',
+    elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 100,
   },
   {
     rank: 2,
-    nickname: '김춘식2',
-    profileImgUrl: '/assets/no-profile.png',
-    badgeImgUrl: '/assets/default.svg',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 95,
   },
   {
     rank: 3,
-    nickname: '김춘식3',
-    profileImgUrl: '/assets/no-profile.png',
-    badgeImgUrl: '/assets/no-profile.png',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 50,
   },
   {
     rank: 4,
-    nickname: '김춘식4',
-    profileImgUrl: '/assets/no-profile.png',
-    badgeImgUrl: '/assets/no-profile.png',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 40,
   },
   {
     rank: 5,
-    nickname: '김춘식5',
-    profileImgUrl: '/assets/no-profile.png',
-    badgeImgUrl: '/assets/no-profile.png',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 20,
   },
   {
     rank: 6,
-    nickname: '김춘식6',
-    profileImgUrl: '/assets/no-profile.png',
-    badgeImgUrl: '/assets/no-profile.png',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 15,
   },
   {
     rank: 7,
-    nickname: '김춘식7',
-    profileImgUrl: '/assets/no-profile.png',
-    badgeImgUrl: '/assets/no-profile.png',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 10,
   },
   {
     rank: 8,
-    nickname: '김춘식8',
-    profileImgUrl: '/assets/no-profile.png',
-    badgeImgUrl: '/assets/no-profile.png',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 8,
   },
   {
     rank: 9,
-    nickname: '김춘식9',
-    profileImgUrl: '/assets/no-profile.png',
-    badgeImgUrl: '/assets/no-profile.png',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 7,
   },
   {
     rank: 10,
-    nickname: '김춘식10',
-    profileImgUrl: '/assets/no-profile.png',
-    badgeImgUrl: '/assets/no-profile.png',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 7,
   },
 ];
--- src/features/Competition/config/mock-chartdata2.ts
+++ src/features/Competition/config/mock-chartdata2.ts
@@ -1,102 +1,80 @@
-export interface chartProps {
+export interface ChartProps {
   rank: number;
   nickname: string;
-  profileImgUrl: string;
-  badgeImgUrl: string;
+  profileUrl: string;
   elapsedTime: string;
   solvedCount: number;
-  value: number;
 }
 
-export const mockChart2: chartProps[] = [
+export const mockChart2: ChartProps[] = [
   {
     rank: 1,
     nickname: '김춘식1',
-    profileImgUrl: 'https://test1.jpeg',
-    badgeImgUrl: 'https://test1.jpeg',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 100,
   },
   {
     rank: 2,
     nickname: '신규유저',
-    profileImgUrl: '/assets/no-profile.png',
-    badgeImgUrl: 'https://test1.jpeg',
-    elapsedTime: '03.24.123',
+    profileUrl: 'https://test1.jpeg',
+    elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 98,
   },
   {
     rank: 3,
-    nickname: '김춘식2',
-    profileImgUrl: 'https://test1.jpeg',
-    badgeImgUrl: 'https://test1.jpeg',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 95,
   },
   {
     rank: 4,
-    nickname: '김춘식3',
-    profileImgUrl: 'https://test1.jpeg',
-    badgeImgUrl: 'https://test1.jpeg',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 50,
   },
   {
     rank: 5,
-    nickname: '김춘식4',
-    profileImgUrl: 'https://test1.jpeg',
-    badgeImgUrl: 'https://test1.jpeg',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 40,
   },
   {
     rank: 6,
-    nickname: '김춘식5',
-    profileImgUrl: '/assets/default.svg',
-    badgeImgUrl: 'https://test1.jpeg',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 20,
   },
   {
     rank: 7,
-    nickname: '김춘식6',
-    profileImgUrl: 'https://test1.jpeg',
-    badgeImgUrl: 'https://test1.jpeg',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 15,
   },
   {
     rank: 8,
-    nickname: '김춘식7',
-    profileImgUrl: 'https://test1.jpeg',
-    badgeImgUrl: 'https://test1.jpeg',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 10,
   },
   {
     rank: 9,
-    nickname: '김춘식8',
-    profileImgUrl: 'https://test1.jpeg',
-    badgeImgUrl: 'https://test1.jpeg',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 8,
   },
   {
     rank: 10,
-    nickname: '김춘식9',
-    profileImgUrl: 'https://test1.jpeg',
-    badgeImgUrl: 'https://test1.jpeg',
+    nickname: '김춘식1',
+    profileUrl: 'https://test1.jpeg',
     elapsedTime: '04.24.123',
     solvedCount: 5,
-    value: 7,
   },
 ];
