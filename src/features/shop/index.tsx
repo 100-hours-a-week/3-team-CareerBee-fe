53078914f8665a99cf1d1233f1c997adbdedd83b
bananackck <emilly03141221@gmail.com>
2025-06-29 16:57:27 +0900

    (Feat) make shop intro

---
+++ src/features/shop/index.tsx
@@ -0,0 +1,11 @@
+import Intro from './components/Intro';
+
+const Shop = () => (
+  <div>
+    <Intro />
+    {/* Additional shop components can be added here */}
+    {/* For example, a product list, cart, etc. */}
+  </div>
+);
+
+export default Shop;
