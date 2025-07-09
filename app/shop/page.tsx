da1bd46409c5a1e5cf8d5e6c8db1f99848645154
bananackck <emilly03141221@gmail.com>
2025-07-09 15:57:45 +0900

    (Feat) move shop code

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,5 +1,11 @@
-import ToBeContinued from '@/src/shared/ui/ToBeContinued';
+import Intro from '@/src/entities/shop/ui/Intro';
+import ProductList from '@/src/entities/shop/ui/ProductList';
 
 export default function Page() {
-  return <ToBeContinued />;
+  return (
+    <div className="overflow-y-auto">
+      <Intro />
+      <ProductList />
+    </div>
+  );
 }
---
+++ src/entities/shop/ui/ProductList.tsx
@@ -0,0 +1,57 @@
+import { ProductProps } from '@/src/features/shop/ui/Product';
+import { TicketType } from '@/src/entities/shop/lib/ticket';
+
+import Product from '@/src/features/shop/ui/Product';
+
+import ticketRed from '@/src/entities/shop/assets/ticket-red.png';
+import ticketGreen from '@/src/entities/shop/assets/ticket-green.png';
+import ticketBlue from '@/src/entities/shop/assets/ticket-blue.png';
+
+import productRed from '@/src/entities/shop/assets/product-red.png';
+import productGreen from '@/src/entities/shop/assets/product-green.png';
+import productBlue from '@/src/entities/shop/assets/product-blue.png';
+
+const productDescriptions: Record<TicketType, string> = {
+  red: '흠.... 뭐하지',
+  green: '하겐다즈',
+  blue: '무무의 오뜨',
+};
+const productImages: Record<TicketType, string> = {
+  red: productRed.src,
+  green: productGreen.src,
+  blue: productBlue.src,
+};
+const ticketConfig: Record<TicketType, ProductProps['ticket']> = {
+  red: {
+    title: '레드 뽑기권',
+    ticketImage: ticketRed.src,
+    point: 5,
+  },
+  green: {
+    title: '그린 뽑기권',
+    ticketImage: ticketGreen.src,
+    point: 10,
+  },
+  blue: {
+    title: '블루 뽑기권',
+    ticketImage: ticketBlue.src,
+    point: 15,
+  },
+};
+
+const ProductList = () => {
+  return (
+    <div className="grid grid-cols-3 gap-8 px-5 mt-20">
+      {Object.entries(ticketConfig).map(([key, ticket]) => (
+        <Product
+          key={key}
+          ticket={ticket}
+          productImage={productImages[key as TicketType]}
+          productDescription={productDescriptions[key as TicketType]}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
