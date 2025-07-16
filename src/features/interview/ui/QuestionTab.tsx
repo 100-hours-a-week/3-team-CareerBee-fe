1a8e4c4d43f1d0b6427fcb696bff355cd6bf1d0f
bananackck <emilly03141221@gmail.com>
2025-07-16 09:33:20 +0900

    (Feat) blur input field on no login

--- src/features/interview/ui/QuestionTab.tsx
+++ src/features/interview/ui/QuestionTab.tsx
@@ -2,8 +2,11 @@ import LongTextForm from '@/src/features/resume/form/ui/longtextForm';
 import { Button } from '@/src/widgets/ui/button';
 
 import { useForm } from 'react-hook-form';
+import { useUserInfo } from '@/src/features/member/model/useUserInfo';
+import Link from 'next/link';
 
 export const QuestionTab = () => {
+  const { data: userInfo } = useUserInfo();
   const {
     control,
     watch,
@@ -18,18 +21,33 @@ export const QuestionTab = () => {
     <>
       {/* 답변 입력 */}
       <form>
-        <LongTextForm
-          title="질문이 ㅇㄹㅇㄴㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㅏㄴ일ㄴ어림ㄴ이ㅏ리ㅏ어리ㅓ아러ㅣ넝러ㅣㄴ어ㅣㅏㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ."
-          controllerName="question"
-          rules={{
-            minLength: [20, '입력을 확인해주세요. (최소 20자)'],
-            maxLength: [500, '입력을 확인해주세요. (최대 500자)'],
-          }}
-          placeholder="답변을 입력해주세요."
-          control={control}
-          errors={errors.question}
-          mainQuestion={true}
-        />
+        <div className="mr-auto font-medium font-bold text-lg mb-2">
+          ㅇㄹㅇㄴㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㅏㄴ일ㄴ어림ㄴ이ㅏ리ㅏ어리ㅓ아러ㅣ넝러ㅣㄴ어ㅣㅏㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ
+        </div>
+        <div className="relative">
+          <LongTextForm
+            title=""
+            controllerName="question"
+            rules={{
+              minLength: [20, '입력을 확인해주세요. (최소 20자)'],
+              maxLength: [500, '입력을 확인해주세요. (최대 500자)'],
+            }}
+            placeholder="답변을 입력해주세요."
+            control={control}
+            errors={errors.question}
+            mainQuestion={true}
+          />
+          {!userInfo?.token && (
+            <div className="absolute inset-0 backdrop-blur-[2px] bg-white/10 flex items-center justify-center z-10 rounded-md">
+              <div className="flex gap-2 items-center text-sm text-gray-700 font-medium">
+                <p>회원만 참여할 수 있어요.</p>
+                <Link href={'/login'} className="underline">
+                  로그인하러 가기
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
 
         <div className="flex w-full justify-center mt-6">
           <Button
