f965b03b77efce60a8ca0a6bab86a1e969d983d2
bananackck <emilly03141221@gmail.com>
2025-07-22 14:15:30 +0900

    (Style) update resume title

--- src/features/resume/form/ui/FormTitle.tsx
+++ src/features/resume/form/ui/FormTitle.tsx
@@ -2,7 +2,7 @@ export const FormTitle = () => {
   return (
     <div className="flex flex-col">
       <div className="text-base font-bold w-full items-start">
-        진척도 조회를 위해 정보를 입력해주세요.
+        이력서 생성을 위해 정보를 입력해주세요.
       </div>
       <p className="text-xs text-text-secondary">해당 정보는 참고용입니다.</p>
     </div>
