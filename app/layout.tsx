ca458cf72b7da0385893df9f6d6dbc9ad7946b66
bananackck <emilly03141221@gmail.com>
2025-07-22 09:40:56 +0900

    (Chore) remove unused font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
 import { AppLayout } from '@/src/app/ui/AppLayout';
@@ -7,16 +6,6 @@ import { Providers } from '@/src/app/ui/Providers';
 import React from 'react';
 import type { Metadata } from 'next';
 import Script from 'next/script';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'CareerBee - IT 커리어 지도',
@@ -32,7 +21,7 @@ if (!kakaoKey) {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className="antialiased">
         <Script
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoKey}&autoload=false&libraries=clusterer,drawing`}
           strategy="beforeInteractive"
